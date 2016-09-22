9a2e0c8962ccb7a61acfd6dafbebea958eacc717
Axel Monroy <xaxelmonroyx@gmail.com>
2016-09-21 21:13:04 -0700

    Tic Tac Toe logic working

--- src/main/java/com/axelmonroy/Game_TicTacToe.java
+++ src/main/java/com/axelmonroy/Game_TicTacToe.java
@@ -1,30 +1,90 @@
 package com.axelmonroy;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by axelmonroyx on 19/09/16.
+ * https://github.com/AxelMonroyX
  */
-public class Game_TicTacToe {
-    public double checkWhoWins;
+class Game_TicTacToe {
+    private String[] actualGame = new String[9];
+    private String lastPlayer;
 
+    void humanSelectPosition(int positionSelectedByHuman) {
 
-    public void humanSelectPosition(int positionSelectedByHuman) {
         selectPosition(positionSelectedByHuman, "human");
     }
 
 
-    public void cpuSelectPosition(int positionSelectedByCPU) {
+    void cpuSelectPosition(int positionSelectedByCPU) {
         selectPosition(positionSelectedByCPU, "cpu");
     }
 
-    private void selectPosition(int positionSelectedByHuman, String who) {
+    private void selectRandomPosition() {
+        Random rnd = new Random();
+        int positionSelectedByCPU = ((int) (rnd.nextDouble() * 9 + 1)) - 1;
+        System.out.println(positionSelectedByCPU);
+        if (actualGame[positionSelectedByCPU] == null) {
+            cpuSelectPosition(positionSelectedByCPU);
+        } else {
+            selectRandomPosition();
+        }
+
     }
 
-    public String checkWhoWins() {
-
-        return "";
+    private void selectPosition(int positionSelected, String who) {
+        if (actualGame[positionSelected] == null) {
+            actualGame[positionSelected] = who;
+            lastPlayer = who;
+        }
     }
 
-    public boolean someoneWins() {
+    String checkWhoWins() {
+        if (someoneWins()) return this.lastPlayer.toUpperCase() + " wins!";
+
+        return "no one wins :( ";
+    }
+
+    boolean someoneWins() {
+        if (fullBoard(actualGame)) return false;
+        List<String> winnerPositions = new LinkedList<String>();
+        winnerPositions.add("0,1,2");
+        winnerPositions.add("0,4,8");
+        winnerPositions.add("0,3,6");
+        winnerPositions.add("1,4,7");
+        winnerPositions.add("2,4,6");
+        winnerPositions.add("2,5,8");
+        winnerPositions.add("3,4,5");
+        int counter;
+        for (int i = 0; i < actualGame.length; i++) {
+            if (actualGame[i].equals(this.lastPlayer)) {
+                counter = 0;
+                for (String position : winnerPositions) {
+                    String[] positionINT = position.split(",");
+                    if (i == Integer.parseInt(positionINT[0])) counter++;
+                    if (i == Integer.parseInt(positionINT[1])) counter++;
+                    if (i == Integer.parseInt(positionINT[2])) counter++;
+
+                }
+                if (counter == 4) {
+                    return true;
+                }
+            }
+
+        }
+
         return false;
     }
+
+    private boolean fullBoard(String[] actualGame) {
+        int counterWithContenent = 0;
+        for (String anActualGame : actualGame) {
+
+            if (anActualGame != null) counterWithContenent++;
+        }
+        return counterWithContenent == actualGame.length;
+    }
+
 }
--- src/test/java/com/axelmonroy/Game_TicTacToe_Test.java
+++ src/test/java/com/axelmonroy/Game_TicTacToe_Test.java
@@ -6,11 +6,12 @@ import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by axelmonroyx on 19/09/16.
+ * https://github.com/AxelMonroyX
  */
 public class Game_TicTacToe_Test {
 
     @Test
-    public void testGame() {
+    public void testGameHumanWins() {
 
         Game_TicTacToe mynewgame = new Game_TicTacToe();
         mynewgame.humanSelectPosition(4);
@@ -19,9 +20,44 @@ public class Game_TicTacToe_Test {
         mynewgame.cpuSelectPosition(1);
         mynewgame.humanSelectPosition(3);
         assertEquals(true, mynewgame.someoneWins());
-        assertEquals("Human Wins !", mynewgame.checkWhoWins());
+        assertEquals("HUMAN wins!", mynewgame.checkWhoWins());
 
     }
+
+    @Test
+    public void testGameHumanWins2() {
+
+        Game_TicTacToe mynewgame = new Game_TicTacToe();
+        mynewgame.humanSelectPosition(4);
+        mynewgame.cpuSelectPosition(8);
+        mynewgame.humanSelectPosition(1);
+        mynewgame.cpuSelectPosition(2);
+        mynewgame.humanSelectPosition(7);
+        assertEquals(true, mynewgame.someoneWins());
+        assertEquals("HUMAN wins!", mynewgame.checkWhoWins());
+
+    }
+
+
+    @Test
+    public void testGameFullBoard() {
+
+        Game_TicTacToe mynewgame = new Game_TicTacToe();
+        mynewgame.humanSelectPosition(4);
+        mynewgame.cpuSelectPosition(1);
+        mynewgame.humanSelectPosition(2);
+        mynewgame.cpuSelectPosition(0);
+        mynewgame.humanSelectPosition(7);
+        mynewgame.cpuSelectPosition(5);
+        mynewgame.humanSelectPosition(3);
+        mynewgame.cpuSelectPosition(6);
+        mynewgame.humanSelectPosition(8);
+        assertEquals(false, mynewgame.someoneWins());
+        assertEquals("no one wins :( ", mynewgame.checkWhoWins());
+
+    }
+
+
 
 
 }
