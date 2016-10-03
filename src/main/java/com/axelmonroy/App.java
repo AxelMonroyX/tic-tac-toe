5b8d8786dfa93fc564b19f15c20414e5ac13ed87
Axel Monroy <xaxelmonroyx@gmail.com>
2016-10-02 19:19:18 -0700

    Change Silly for Smart in comments and title

--- src/main/java/com/axelmonroy/App.java
+++ src/main/java/com/axelmonroy/App.java
@@ -3,13 +3,13 @@ package com.axelmonroy;
 import javax.swing.*;
 
 /**
- * Silly  Tic-Tac-Toe - By Axel!
+ * Smart  Tic-Tac-Toe - By Axel!
  *
  */
 public class App 
 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Silly  Tic-Tac-Toe - By Axel");
+        JFrame frame = new JFrame("Smart Tic-Tac-Toe - By Axel");
         frame.setContentPane(new Client_Tic_Tac_Toe().JPanelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
